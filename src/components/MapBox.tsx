4de5cbd35e5a754e7f7c525d85e04f87ea4afa93
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-11-27 00:08:04 -0500

    weather map layered on top of regular leaflet map

--- src/components/MapBox.tsx
+++ src/components/MapBox.tsx
@@ -1,16 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 
 const MapBox = (): JSX.Element => {
   return (
     <MapContainer
       center={[40.29066735, -75.21070232492798]}
-      zoom={13}
+      zoom={10}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <TileLayer
+        maxZoom={10}
+        opacity={3}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=021e75b0e3380e236b4ff6031ae2dde4"
       />
       <Marker position={[40.29066735, -75.21070232492798]}>
         <Popup>
